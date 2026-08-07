4ad8ee37dbe18665146351a89859b287fce9b89a
IT剣士 <miyabitatibanatkh@gmail.com>
2026-08-08 00:23:24 +0900

    complate create_video_writer function

--- src/video/writer.py
+++ src/video/writer.py
@@ -2,6 +2,9 @@ import cv2
 from pathlib import Path
 
 def create_video_writer(output_path: str | Path, fps: float, width: int, height: int) -> cv2.VideoWriter:
-    #  TODO
 
-    pass
+    path = Path(output_path)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    writer = cv2.VideoWriter(str(path), fourcc, fps, (width, height))
+
+    return writer
