26f94f6cac6de21c4716ab55a692c8cb6e1f0490
IT剣士 <miyabitatibanatkh@gmail.com>
2026-08-08 07:11:44 +0900

    add geometry.py and add midpoint function

---
+++ src/analysis/geometry.py
@@ -0,0 +1,8 @@
+# import
+# import
+
+
+def midpoint(point1: tuple[float, float], point2: tuple[float, float]) -> tuple[float, float]:
+    xm = (point1[0] + point2[0]) / 2
+    ym = (point1[1] + point2[1]) / 2
+    return (xm, ym)
