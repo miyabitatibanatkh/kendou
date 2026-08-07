6f23f1f1953fafd915ec1e6e61dbebc65fe9dc91
IT剣士 <miyabitatibanatkh@gmail.com>
2026-08-08 01:28:48 +0900

    fix create_video_writer function

--- src/video/writer.py
+++ src/video/writer.py
@@ -8,8 +8,13 @@ def create_video_writer(output_path: str | Path, fps: float, width: int, height:
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     writer = cv2.VideoWriter(str(path), fourcc, fps, (width, height))
 
-    return writer
+    if  writer.isOpened():
+        return writer
+    else:
+        writer.release()
+        raise ValueError(f"Failed to create a video writer for the output file at {output_path}. Check the path and format.")
 
 
 def write_frame(writer: cv2.VideoWriter, frame: cv2.Mat) -> None:
     writer.write(frame)
+
